fd56e0ceed9662eaca855aca76d69913e9667b8a
TheCarry33 <tomreljin@gmail.com>
2026-04-13 16:51:12 -0400

    Added a screen when setting button is pressed which asks for the master password.

--- app/src/main/java/com/google/mediapipe/examples/poselandmarker/MainActivity.kt
+++ app/src/main/java/com/google/mediapipe/examples/poselandmarker/MainActivity.kt
@@ -26,6 +26,7 @@ import com.google.mediapipe.examples.poselandmarker.databinding.ActivityMainBind
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
     private val viewModel : MainViewModel by viewModels()
+    private var isProgrammaticNavigation = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +36,35 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
-        activityMainBinding.navigation.setupWithNavController(navController)
+
+        activityMainBinding.navigation.setOnItemSelectedListener { item ->
+
+            if (isProgrammaticNavigation) {
+                isProgrammaticNavigation = false
+                return@setOnItemSelectedListener true
+            }
+
+            when (item.itemId) {
+
+                R.id.gallery_fragment -> {
+
+                    showPasswordDialog { success ->
+                        if (success) {
+                            isProgrammaticNavigation = true
+                            navController.navigate(R.id.gallery_fragment)
+                            activityMainBinding.navigation.selectedItemId = R.id.gallery_fragment
+                        }
+                    }
+
+                    false
+                }
+
+                else -> {
+                    navController.navigate(item.itemId)
+                    true
+                }
+            }
+        }
         activityMainBinding.navigation.setOnNavigationItemReselectedListener {
             // ignore the reselection
         }
@@ -43,5 +72,41 @@ class MainActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         finish()
+    }
+
+    private fun showPasswordDialog(onResult: (Boolean) -> Unit) {
+
+        val editText = android.widget.EditText(this)
+        editText.hint = "Enter master password"
+        editText.inputType =
+            android.text.InputType.TYPE_CLASS_TEXT or android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+        val dialog = androidx.appcompat.app.AlertDialog.Builder(this)
+            .setTitle("Enter Master Password")
+            .setView(editText)
+            .setCancelable(false)
+            .setPositiveButton("OK", null)
+            .setNegativeButton("Cancel") { d, _ ->
+                d.dismiss()
+                onResult(false)
+            }
+            .create()
+
+        dialog.setOnShowListener {
+            val button = dialog.getButton(androidx.appcompat.app.AlertDialog.BUTTON_POSITIVE)
+
+            button.setOnClickListener {
+                val input = editText.text.toString()
+
+                if (input == "password") {
+                    dialog.dismiss()
+                    onResult(true)
+                } else {
+                    editText.error = "Incorrect password. Try again."
+                }
+            }
+        }
+
+        dialog.show()
     }
 }
